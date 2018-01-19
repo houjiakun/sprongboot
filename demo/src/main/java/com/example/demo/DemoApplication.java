9cd019177a18fba064c643358032f0225a5f8fd4
houjiakun <531852181@qq.com>
2018-01-19 14:42:11 +0800

    spring boot demo

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+	@Value(value = "${book.author}")
+	private String bookAuthor;
+	@Value("${book.name}")
+	private String bookName;
+	@Value("${book.pinyin}")
+	private String bookPinYin;
+
+	@Autowired
+	private BookBean bookBean;
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@RequestMapping(value="/",produces = "text/plain;charset=UTF-8")
+	public String index(){
+		return "my spring boot demo!" + bookAuthor + " "+ bookName+ " " + bookBean.getAuthor();
+	}
+
+
+}
